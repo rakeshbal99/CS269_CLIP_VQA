3800c1ef227cb6024325ba167d08f4c69d63fdc8
Rakesh Bal <rakeshbal99@gmail.com>
2022-05-03 16:57:49 -0700

    Add data Preprocess

---
+++ dataProcess.py
@@ -0,0 +1,35 @@
+import json
+annotations_path = "v2_mscoco_val2014_annotations.json"
+questions_path = "v2_OpenEnded_mscoco_val2014_questions.json"
+
+f = open(annotations_path)
+anno = json.load(f)
+f.close()
+
+f = open(questions_path)
+ques = json.load(f)
+f.close()
+
+split_len = 50000
+
+new_anno = {}
+
+for key in anno:
+    if key == "annotations":
+        new_anno[key] = anno[key][:split_len]
+    else:
+        new_anno[key] = anno[key]
+
+new_ques = {}
+
+for key in ques:
+    if key == "questions":
+        new_ques[key] = ques[key][:split_len]
+    else:
+        new_ques[key] = ques[key]
+
+with open( "new_" + annotations_path, "w") as outfile:
+    json.dump(new_anno, outfile)
+
+with open( "new_" + questions_path, "w") as outfile:
+    json.dump(new_ques, outfile)
